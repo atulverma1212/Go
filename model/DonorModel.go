7f66492ef5398ea01c6a58651a59093ccad5ce0a
Atul Verma <verma.av1997@gmail.com>
2018-11-20 23:20:54 +0530

    Changed dob to DOB

--- model/DonorModel.go
+++ model/DonorModel.go
@@ -3,7 +3,7 @@ package model
 type Donor struct {
 	Id         uint64 `bson:"_id" json:"_id"`
 	Name       string `bson:"name" json:"name"`
-	Age        int    `bson:"age" json:"age"`
+	DOB        string   `bson:"dob" json:"dob"`
 	Phone      string `bson:"phone" json:"phone"`
 	Email      string `bson:"email" json:"email"`
 	BloodGroup string `bson:"blood_group" json:"blood_group"`
